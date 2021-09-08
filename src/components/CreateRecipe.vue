c052eaf5a51945b1c2386251a9b1adedaed8fffa
PepCarmona <pep.carmona.coll@gmail.com>
2021-09-08 15:53:11 +0200

    extract recipe inputs into separate component

---
+++ src/components/CreateRecipe.vue
@@ -0,0 +1,207 @@
+<template>
+    <h3 v-if="recipe">Edit recipe nº {{ index + 1 }}</h3>
+    <h3 v-else>New recipe</h3>
+
+    <label for="titleInput">Title: </label>
+    <input v-model="newRecipe.title" type="text" id="titleInput" />
+
+    <label for="descriptionInput">Description: </label>
+    <textarea v-model="newRecipe.description" id="descriptionInput" />
+
+    <div>
+        <label for="prepTimeInput">Preparation Time: </label>
+        <input
+            v-model="newRecipe.time.preparation"
+            type="number"
+            min="0"
+            id="prepTimeInput"
+        />
+
+        <label for="cookTimeInput">Cooking Time: </label>
+        <input
+            v-model="newRecipe.time.cooking"
+            type="number"
+            min="0"
+            id="cookTimeInput"
+        />
+    </div>
+
+    <label for="servingsInput">Servings: </label>
+    <input
+        v-model="newRecipe.servings"
+        type="number"
+        min="0"
+        id="servingsInput"
+    />
+
+    <div>
+        Ingredients:
+        <div
+            v-for="(ingredient, index) in newRecipe.ingredients"
+            :key="ingredient._id"
+        >
+            <input v-model="ingredient.quantity" type="number" min="0" />
+            <input v-model="ingredient.units" type="text" />
+            <input v-model="ingredient.name" type="text" />
+            <button @click="deleteIngredient(index)">Delete</button>
+        </div>
+        <button @click="addIngredient">Add</button>
+    </div>
+
+    <div>
+        Instructions:
+        <div v-for="(step, index) in newRecipe.instructions" :key="step._id">
+            Step {{ step.position }} -
+            <textarea v-model="step.content" />
+            <button @click="deleteStep(index)">Delete</button>
+        </div>
+        <button @click="addStep">Add</button>
+    </div>
+
+    <div>
+        Tags:
+        <div v-for="(tag, index) in newRecipe.tags" :key="tag._id">
+            <input v-model="tag.value" type="text" />
+            <button @click="deleteTag(index)">Delete</button>
+        </div>
+        <button @click="addTag">Add</button>
+    </div>
+
+    <button v-if="recipe" @click="editRecipe">Save</button>
+    <button v-else @click="addRecipe">Save</button>
+</template>
+
+<script lang="ts">
+import { URI } from '@/api/config';
+import {
+    IngredientClass,
+    Recipe,
+    RecipeClass,
+    StepClass,
+    TagClass,
+} from '@/api/recipes/recipe';
+import { AxiosStatic } from 'axios';
+import {
+    computed,
+    defineComponent,
+    inject,
+    onMounted,
+    PropType,
+    ref,
+} from 'vue';
+
+export default defineComponent({
+    name: 'CreateRecipe',
+
+    props: {
+        recipe: Object as PropType<Recipe>,
+        index: Number,
+    },
+
+    emits: ['show-all-recipes'],
+
+    setup(props, { emit }) {
+        const axios: AxiosStatic | undefined = inject('axios');
+
+        const newRecipe = ref<Recipe>(new RecipeClass());
+
+        onMounted(() => {
+            if (props.recipe) {
+                newRecipe.value = props.recipe;
+            }
+        });
+
+        const sanitizedRecipe = computed<Recipe>(() => {
+            const sanitized = newRecipe.value;
+            sanitized.ingredients = sanitized.ingredients.filter(
+                (ingredient) => !!ingredient.quantity || !!ingredient.name
+            );
+            sanitized.instructions = sanitized.instructions.filter(
+                (step) => !!step.content
+            );
+            sanitized.instructions.map(
+                (step, index) => (step.position = index + 1)
+            );
+            sanitized.tags = sanitized.tags.filter((tag) => !!tag.value);
+            return sanitized;
+        });
+
+        function addIngredient() {
+            newRecipe.value.ingredients.push(new IngredientClass());
+        }
+
+        function addStep() {
+            const newStep = new StepClass();
+            newStep.position = newRecipe.value.instructions.length + 1;
+
+            newRecipe.value.instructions.push(newStep);
+        }
+
+        function addTag() {
+            newRecipe.value.tags.push(new TagClass());
+        }
+
+        function deleteIngredient(index: number) {
+            newRecipe.value.ingredients.splice(index, 1);
+        }
+
+        function deleteStep(index: number) {
+            newRecipe.value.instructions.splice(index, 1);
+        }
+
+        function deleteTag(index: number) {
+            newRecipe.value.tags.splice(index, 1);
+        }
+
+        function addRecipe() {
+            if (sanitizedRecipe.value.title === '') {
+                window.alert('Cannot save recipes with empty title');
+                return;
+            }
+
+            if (
+                sanitizedRecipe.value.ingredients.length === 0 ||
+                sanitizedRecipe.value.instructions.length === 0
+            ) {
+                const confirm = window.confirm(
+                    'Ingredients and/or instructions are empty. Do you stil want to save this recipe?'
+                );
+                if (!confirm) {
+                    return;
+                }
+            }
+
+            axios
+                ?.post(URI.recipes.add, sanitizedRecipe.value)
+                .then(() => {
+                    emit('show-all-recipes');
+                })
+                .catch((err) => console.error(err));
+        }
+
+        function editRecipe() {
+            const url = new URL(URI.recipes.update);
+            url.searchParams.append('id', newRecipe.value._id!);
+
+            axios
+                ?.put(url.toString(), sanitizedRecipe.value)
+                .then(() => {
+                    emit('show-all-recipes');
+                })
+                .catch((err) => console.error(err));
+        }
+
+        return {
+            newRecipe,
+            addIngredient,
+            addStep,
+            addTag,
+            deleteIngredient,
+            deleteStep,
+            deleteTag,
+            addRecipe,
+            editRecipe,
+        };
+    },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,81 +7,7 @@
         </div>
         <div v-if="isSearching">Loading...</div>
         <div v-if="selectedView === 'add'">
-            <h3>New recipe</h3>
-
-            <label for="titleInput">Title: </label>
-            <input v-model="newRecipe.title" type="text" id="titleInput" />
-
-            <label for="descriptionInput">Description: </label>
-            <textarea v-model="newRecipe.description" id="descriptionInput" />
-
-            <div>
-                <label for="prepTimeInput">Preparation Time: </label>
-                <input
-                    v-model="newRecipe.time.preparation"
-                    type="number"
-                    min="0"
-                    id="prepTimeInput"
-                />
-
-                <label for="cookTimeInput">Cooking Time: </label>
-                <input
-                    v-model="newRecipe.time.cooking"
-                    type="number"
-                    min="0"
-                    id="cookTimeInput"
-                />
-            </div>
-
-            <label for="servingsInput">Servings: </label>
-            <input
-                v-model="newRecipe.servings"
-                type="number"
-                min="0"
-                id="servingsInput"
-            />
-
-            <div>
-                Ingredients:
-                <div
-                    v-for="(ingredient, index) in newRecipe.ingredients"
-                    :key="ingredient._id"
-                >
-                    <input
-                        v-model="ingredient.quantity"
-                        type="number"
-                        min="0"
-                    />
-                    <input v-model="ingredient.units" type="text" />
-                    <input v-model="ingredient.name" type="text" />
-                    <button @click="deleteIngredient(index)">Delete</button>
-                </div>
-                <button @click="addIngredient">Add</button>
-            </div>
-
-            <div>
-                Instructions:
-                <div
-                    v-for="(step, index) in newRecipe.instructions"
-                    :key="step._id"
-                >
-                    Step {{ step.position }} -
-                    <textarea v-model="step.content" />
-                    <button @click="deleteStep(index)">Delete</button>
-                </div>
-                <button @click="addStep">Add</button>
-            </div>
-
-            <div>
-                Tags:
-                <div v-for="(tag, index) in newRecipe.tags" :key="tag._id">
-                    <input v-model="tag.value" type="text" />
-                    <button @click="deleteTag(index)">Delete</button>
-                </div>
-                <button @click="addTag">Add</button>
-            </div>
-
-            <button @click="addRecipe">Save</button>
+            <CreateRecipe @show-all-recipes="showAllRecipes" />
         </div>
         <div v-if="selectedView === 'view' && !isSearching">
             <h3>Total number of recipes: {{ recipes.length }}</h3>
@@ -170,113 +96,31 @@
             </div>
         </div>
         <div v-if="selectedView === 'edit'">
-            <h3>Edit recipe nº {{ selectedIndex + 1 }}</h3>
-
-            <label for="titleInput">Title: </label>
-            <input v-model="selectedRecipe.title" type="text" id="titleInput" />
-
-            <label for="descriptionInput">Description: </label>
-            <textarea
-                v-model="selectedRecipe.description"
-                id="descriptionInput"
+            <CreateRecipe
+                :recipe="selectedRecipe"
+                :index="selectedIndex"
+                @show-all-recipes="showAllRecipes"
             />
-
-            <div>
-                <label for="prepTimeInput">Preparation Time: </label>
-                <input
-                    v-model="selectedRecipe.time.preparation"
-                    type="number"
-                    min="0"
-                    id="prepTimeInput"
-                />
-
-                <label for="cookTimeInput">Cooking Time: </label>
-                <input
-                    v-model="selectedRecipe.time.cooking"
-                    type="number"
-                    min="0"
-                    id="cookTimeInput"
-                />
-            </div>
-
-            <label for="servingsInput">Servings: </label>
-            <input
-                v-model="selectedRecipe.servings"
-                type="number"
-                min="0"
-                id="servingsInput"
-            />
-
-            <div>
-                Ingredients:
-                <div
-                    v-for="(ingredient, index) in selectedRecipe.ingredients"
-                    :key="ingredient._id"
-                >
-                    <input
-                        v-model="ingredient.quantity"
-                        type="number"
-                        min="0"
-                    />
-                    <input v-model="ingredient.units" type="text" />
-                    <input v-model="ingredient.name" type="text" />
-                    <button @click="deleteIngredient(index, selectedRecipe)">
-                        Delete
-                    </button>
-                </div>
-                <button @click="addIngredient($event, selectedRecipe)">
-                    Add
-                </button>
-            </div>
-
-            <div>
-                Instructions:
-                <div
-                    v-for="(step, index) in selectedRecipe.instructions"
-                    :key="step._id"
-                >
-                    Step {{ step.position }} -
-                    <textarea v-model="step.content" />
-                    <button @click="deleteStep(index, selectedRecipe)">
-                        Delete
-                    </button>
-                </div>
-                <button @click="addStep($event, selectedRecipe)">Add</button>
-            </div>
-
-            <div>
-                Tags:
-                <div v-for="(tag, index) in selectedRecipe.tags" :key="tag">
-                    <input v-model="tag.value" type="text" />
-                    <button @click="deleteTag(index, selectedRecipe)">
-                        Delete
-                    </button>
-                </div>
-                <button @click="addTag($event, selectedRecipe)">Add</button>
-            </div>
-
-            <button @click="editRecipe">Save</button>
         </div>
     </div>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, inject, onMounted, ref } from 'vue';
-import {
-    IngredientClass,
-    Recipe,
-    RecipeClass,
-    StepClass,
-    TagClass,
-} from '@/api/recipes/recipe';
+import { Recipe, RecipeClass } from '@/api/recipes/recipe';
 import { AxiosResponse, AxiosStatic } from 'axios';
 import { URI } from '@/api/config/index';
 import { useRouter } from 'vue-router';
+import CreateRecipe from '@/components/CreateRecipe.vue';
 
 type View = 'add' | 'edit' | 'view' | 'search';
 
 export default defineComponent({
     name: 'Home',
+
+    components: {
+        CreateRecipe,
+    },
 
     setup() {
         const router = useRouter();
@@ -310,50 +154,9 @@ export default defineComponent({
             return recipes.value[selectedIndex.value];
         });
 
-        const sanitizedRecipe = computed<Recipe>(() => {
-            const sanitized = selectedRecipe.value ?? newRecipe.value;
-            sanitized.ingredients = sanitized.ingredients.filter(
-                (ingredient) => !!ingredient.quantity || !!ingredient.name
-            );
-            sanitized.instructions = sanitized.instructions.filter(
-                (step) => !!step.content
-            );
-            sanitized.instructions.map(
-                (step, index) => (step.position = index + 1)
-            );
-            sanitized.tags = sanitized.tags.filter((tag) => !!tag.value);
-            return sanitized;
-        });
-
         function showAddRecipe() {
             selectedView.value = selectedView.value === 'add' ? null : 'add';
             newRecipe.value = new RecipeClass();
-        }
-
-        function addRecipe() {
-            if (sanitizedRecipe.value.title === '') {
-                window.alert('Cannot save recipes with empty title');
-                return;
-            }
-
-            if (
-                sanitizedRecipe.value.ingredients.length === 0 ||
-                sanitizedRecipe.value.instructions.length === 0
-            ) {
-                const confirm = window.confirm(
-                    'Ingredients and/or instructions are empty. Do you stil want to save this recipe?'
-                );
-                if (!confirm) {
-                    return;
-                }
-            }
-
-            axios
-                ?.post(URI.recipes.add, sanitizedRecipe.value)
-                .then(() => {
-                    showAllRecipes();
-                })
-                .catch((err) => console.error(err));
         }
 
         function showAllRecipes() {
@@ -391,23 +194,6 @@ export default defineComponent({
         function showEditRecipe(index: number) {
             selectedView.value = 'edit';
             selectedIndex.value = index;
-        }
-
-        function editRecipe() {
-            if (selectedIndex.value === null || selectedRecipe.value === null) {
-                console.error('No selected recipe');
-                return;
-            }
-
-            const url = new URL(URI.recipes.update);
-            url.searchParams.append('id', selectedRecipe.value._id!);
-
-            axios
-                ?.put(url.toString(), sanitizedRecipe.value)
-                .then(() => {
-                    showAllRecipes();
-                })
-                .catch((err) => console.error(err));
         }
 
         function deleteRecipe(index: number) {
@@ -454,63 +240,16 @@ export default defineComponent({
             });
         }
 
-        function addIngredient(event: Event, modifyingRecipe?: Recipe) {
-            let recipe = modifyingRecipe ?? newRecipe.value;
-
-            recipe.ingredients.push(new IngredientClass());
-        }
-
-        function addStep(event: Event, modifyingRecipe?: Recipe) {
-            let recipe = modifyingRecipe ?? newRecipe.value;
-
-            const newStep = new StepClass();
-            newStep.position = recipe.instructions.length + 1;
-
-            recipe.instructions.push(newStep);
-        }
-
-        function addTag(event: Event, modifyingRecipe?: Recipe) {
-            let recipe = modifyingRecipe ?? newRecipe.value;
-
-            recipe.tags.push(new TagClass());
-        }
-
-        function deleteIngredient(index: number, modifyingRecipe?: Recipe) {
-            let recipe = modifyingRecipe ?? newRecipe.value;
-
-            recipe.ingredients.splice(index, 1);
-        }
-
-        function deleteStep(index: number, modifyingRecipe?: Recipe) {
-            let recipe = modifyingRecipe ?? newRecipe.value;
-
-            recipe.instructions.splice(index, 1);
-        }
-
-        function deleteTag(index: number, modifyingRecipe?: Recipe) {
-            let recipe = modifyingRecipe ?? newRecipe.value;
-
-            recipe.tags.splice(index, 1);
-        }
-
         return {
             ...data,
             showAddRecipe,
             showAllRecipes,
             showSearchRecipe,
             searchRecipeByTitle,
-            addRecipe,
             showEditRecipe,
-            editRecipe,
             deleteRecipe,
             selectedRecipe,
             openRecipeDetails,
-            addIngredient,
-            addStep,
-            addTag,
-            deleteIngredient,
-            deleteStep,
-            deleteTag,
         };
     },
 });
