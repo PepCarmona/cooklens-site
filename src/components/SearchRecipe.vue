45459f60af36a22f72186678b4298628b9829d09
PepCarmona <pep.carmona.coll@gmail.com>
2021-09-08 23:17:44 +0200

    Add search by ingredient and tag

--- src/components/SearchRecipe.vue
+++ src/components/SearchRecipe.vue
@@ -1,11 +1,27 @@
 <template>
-    <h3>Search by title</h3>
+    <h3>Search by {{ searchType }}</h3>
 
     <input
+        ref="searchInput"
         type="text"
         id="search"
-        @input="searchRecipeByTitle($event.target.value)"
+        @input="search($event.target.value)"
     />
+
+    <div>
+        <button v-if="searchType !== 'title'" @click="changeSearch('title')">
+            Search by title
+        </button>
+        <button
+            v-if="searchType !== 'ingredient'"
+            @click="changeSearch('ingredient')"
+        >
+            Search by ingredient
+        </button>
+        <button v-if="searchType !== 'tag'" @click="changeSearch('tag')">
+            Search by tag
+        </button>
+    </div>
 
     <div v-for="recipe in searchResult" :key="recipe._id">
         <div>{{ recipe.title }}</div>
@@ -15,7 +31,9 @@
 
 <script lang="ts">
 import { Recipe } from '@/api/recipes/recipe';
-import { defineComponent, PropType, ref } from 'vue';
+import { defineComponent, onMounted, PropType, ref } from 'vue';
+
+type SearchType = 'title' | 'ingredient' | 'tag';
 
 export default defineComponent({
     name: 'SearchRecipe',
@@ -30,18 +48,74 @@ export default defineComponent({
     setup(props) {
         const searchResult = ref<Recipe[]>([]);
 
-        function searchRecipeByTitle(value: string) {
+        const searchType = ref<SearchType>('title');
+
+        const searchInput = ref<HTMLInputElement>();
+
+        onMounted(() => searchInput.value?.focus());
+
+        function searchByTitle(searchQuery: string) {
             searchResult.value = props.recipes.filter(
                 (recipe) =>
-                    value.length > 0 &&
-                    recipe.title.substr(0, value.length).toLowerCase() ===
-                        value.toLowerCase()
+                    searchQuery.length > 0 &&
+                    recipe.title
+                        .toLowerCase()
+                        .search(searchQuery.toLowerCase()) > -1
             );
+        }
+
+        function searchByIngredient(searchQuery: string) {
+            searchResult.value = props.recipes.filter(
+                (recipe) =>
+                    searchQuery.length > 0 &&
+                    recipe.ingredients.some(
+                        (ingredient) =>
+                            ingredient.name
+                                .toLowerCase()
+                                .search(searchQuery.toLowerCase()) > -1
+                    )
+            );
+        }
+        function searchByTag(searchQuery: string) {
+            searchResult.value = props.recipes.filter(
+                (recipe) =>
+                    searchQuery.length > 0 &&
+                    recipe.tags.some(
+                        (tag) =>
+                            tag.value
+                                .toLowerCase()
+                                .search(searchQuery.toLowerCase()) > -1
+                    )
+            );
+        }
+
+        function search(searchQuery: string) {
+            switch (searchType.value) {
+                case 'title':
+                    searchByTitle(searchQuery);
+                    break;
+                case 'ingredient':
+                    searchByIngredient(searchQuery);
+                    break;
+                case 'tag':
+                    searchByTag(searchQuery);
+                    break;
+            }
+        }
+
+        function changeSearch(type: SearchType) {
+            searchType.value = type;
+            if (searchInput.value) {
+                search(searchInput.value.value);
+            }
         }
 
         return {
             searchResult,
-            searchRecipeByTitle,
+            search,
+            searchType,
+            changeSearch,
+            searchInput,
         };
     },
 });
