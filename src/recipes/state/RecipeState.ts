44cc1935def3d465d45862c47ccd65dca2fe3d17
PepCarmona <pep.carmona.coll@gmail.com>
2022-02-23 22:53:16 +0100

    encapaulate states inside create functions

--- src/recipes/state/RecipeState.ts
+++ src/recipes/state/RecipeState.ts
@@ -13,141 +13,142 @@ import {
 	SearchType,
 } from 'cooklens-types';
 
-const recipeService = new RecipesEndpoint();
-
-const { checkIfNextPageExists, goToPage } = usePaginationState();
-const { authenticatedUser } = useAuthenticationState();
-
-const isLoading = ref(false);
-const isOwnRecipe = ref(false);
-const recipe = ref<Recipe>(new RecipeClass());
-const canModifyServings = ref(false);
-const modifiedServings = ref<number | null>(null);
-const recipes = ref<Recipe[]>([]);
-const searchQuery = reactive<SearchQuery>({ type: 'title', text: '' });
-
-function isFavoriteRecipe(recipe: Recipe): boolean {
-	if (!authenticatedUser.value || !authenticatedUser.value.favRecipes) {
-		return false;
-	}
-	return authenticatedUser.value.favRecipes.includes(recipe._id!);
-}
-
-function addRecipe(recipe: Recipe): Promise<Recipe> {
-	isLoading.value = true;
-
-	recipe.author = authenticatedUser.value?._id;
-
-	return recipeService
-		.addRecipe(recipe)
-		.finally(() => (isLoading.value = false));
-}
-
-function editRecipe(_recipe: Recipe): Promise<Recipe> {
-	isLoading.value = true;
-
-	return recipeService
-		.editRecipe(_recipe)
-		.then((editedRecipe) => (recipe.value = editedRecipe))
-		.finally(() => (isLoading.value = false));
-}
-
-async function editRating(value: number) {
-	recipe.value.rating = value;
-
-	await recipeService.editRecipe(recipe.value);
-}
-
-function importRecipe(url: string) {
-	isLoading.value = true;
-
-	return recipeService
-		.importRecipe(url)
-		.finally(() => (isLoading.value = false));
-}
-
-function setSearch(type: SearchType, text: string) {
-	searchQuery.type = type;
-	searchQuery.text = text;
-}
-
-function searchRecipes(page = 1, limit = 10) {
-	isLoading.value = true;
-
-	return recipeService
-		.searchRecipes(page, limit, searchQuery.type, searchQuery.text)
-		.then((paginatedRecipes) => {
-			goToPage(page);
-
-			checkIfNextPageExists(paginatedRecipes.next);
-
-			recipes.value = paginatedRecipes.result;
-		})
-		.finally(() => (isLoading.value = false));
-}
-
-function getRecipe(id: string) {
-	isLoading.value = true;
-
-	return recipeService
-		.getRecipe(id)
-		.then((resultRecipe) => {
-			// TODO: remove when all recipes in db are sanitized
-			if (resultRecipe.time) {
-				resultRecipe.time.preparation = resultRecipe.time.preparation ?? 0;
-			}
-
-			recipe.value = resultRecipe;
-
-			canModifyServings.value = recipe.value.ingredients.some(
-				(ingredient) => ingredient.quantity && ingredient.quantity > 0
-			);
-
-			if (canModifyServings.value) {
-				modifiedServings.value = recipe.value.servings;
-			}
-
-			isOwnRecipe.value =
-				!!resultRecipe.author &&
-				authenticatedUser.value?._id === (resultRecipe.author as UserInfo)._id;
-		})
-		.finally(() => (isLoading.value = false));
-}
-
-function getRandomRecipe() {
-	isLoading.value = true;
-
-	return recipeService
-		.getRandomRecipe()
-		.then((randomRecipe) => {
-			recipe.value = randomRecipe;
-
-			canModifyServings.value = recipe.value.ingredients.some(
-				(ingredient) => ingredient.quantity && ingredient.quantity > 0
-			);
-
-			if (canModifyServings.value) {
-				modifiedServings.value = recipe.value.servings;
-			}
-		})
-		.finally(() => (isLoading.value = false));
-}
-
-function deleteRecipe(recipe: Recipe) {
-	isLoading.value = true;
-
-	return recipeService
-		.deleteRecipe(recipe)
-		.finally(() => (isLoading.value = false));
-}
-
-function getMainImageUrl(recipe: Recipe | undefined): string {
-	return recipe && recipe.images && recipe.images?.length > 0
-		? recipe.images[0]
-		: '';
-}
-
 export default function useRecipeState() {
+	const recipeService = new RecipesEndpoint();
+
+	const { checkIfNextPageExists, goToPage } = usePaginationState();
+	const { authenticatedUser } = useAuthenticationState();
+
+	const isLoading = ref(false);
+	const isOwnRecipe = ref(false);
+	const recipe = ref<Recipe>(new RecipeClass());
+	const canModifyServings = ref(false);
+	const modifiedServings = ref<number | null>(null);
+	const recipes = ref<Recipe[]>([]);
+	const searchQuery = reactive<SearchQuery>({ type: 'title', text: '' });
+
+	function isFavoriteRecipe(recipe: Recipe): boolean {
+		if (!authenticatedUser.value || !authenticatedUser.value.favRecipes) {
+			return false;
+		}
+		return authenticatedUser.value.favRecipes.includes(recipe._id!);
+	}
+
+	function addRecipe(recipe: Recipe): Promise<Recipe> {
+		isLoading.value = true;
+
+		recipe.author = authenticatedUser.value?._id;
+
+		return recipeService
+			.addRecipe(recipe)
+			.finally(() => (isLoading.value = false));
+	}
+
+	function editRecipe(_recipe: Recipe): Promise<Recipe> {
+		isLoading.value = true;
+
+		return recipeService
+			.editRecipe(_recipe)
+			.then((editedRecipe) => (recipe.value = editedRecipe))
+			.finally(() => (isLoading.value = false));
+	}
+
+	async function editRating(value: number) {
+		recipe.value.rating = value;
+
+		await recipeService.editRecipe(recipe.value);
+	}
+
+	function importRecipe(url: string) {
+		isLoading.value = true;
+
+		return recipeService
+			.importRecipe(url)
+			.finally(() => (isLoading.value = false));
+	}
+
+	function setSearch(type: SearchType, text: string) {
+		searchQuery.type = type;
+		searchQuery.text = text;
+	}
+
+	function searchRecipes(page = 1, limit = 10) {
+		isLoading.value = true;
+
+		return recipeService
+			.searchRecipes(page, limit, searchQuery.type, searchQuery.text)
+			.then((paginatedRecipes) => {
+				goToPage(page);
+
+				checkIfNextPageExists(paginatedRecipes.next);
+
+				recipes.value = paginatedRecipes.result;
+			})
+			.finally(() => (isLoading.value = false));
+	}
+
+	function getRecipe(id: string) {
+		isLoading.value = true;
+
+		return recipeService
+			.getRecipe(id)
+			.then((resultRecipe) => {
+				// TODO: remove when all recipes in db are sanitized
+				if (resultRecipe.time) {
+					resultRecipe.time.preparation = resultRecipe.time.preparation ?? 0;
+				}
+
+				recipe.value = resultRecipe;
+
+				canModifyServings.value = recipe.value.ingredients.some(
+					(ingredient) => ingredient.quantity && ingredient.quantity > 0
+				);
+
+				if (canModifyServings.value) {
+					modifiedServings.value = recipe.value.servings;
+				}
+
+				isOwnRecipe.value =
+					!!resultRecipe.author &&
+					authenticatedUser.value?._id ===
+						(resultRecipe.author as UserInfo)._id;
+			})
+			.finally(() => (isLoading.value = false));
+	}
+
+	function getRandomRecipe() {
+		isLoading.value = true;
+
+		return recipeService
+			.getRandomRecipe()
+			.then((randomRecipe) => {
+				recipe.value = randomRecipe;
+
+				canModifyServings.value = recipe.value.ingredients.some(
+					(ingredient) => ingredient.quantity && ingredient.quantity > 0
+				);
+
+				if (canModifyServings.value) {
+					modifiedServings.value = recipe.value.servings;
+				}
+			})
+			.finally(() => (isLoading.value = false));
+	}
+
+	function deleteRecipe(recipe: Recipe) {
+		isLoading.value = true;
+
+		return recipeService
+			.deleteRecipe(recipe)
+			.finally(() => (isLoading.value = false));
+	}
+
+	function getMainImageUrl(recipe: Recipe | undefined): string {
+		return recipe && recipe.images && recipe.images?.length > 0
+			? recipe.images[0]
+			: '';
+	}
+
 	return {
 		isLoading: readonly(isLoading),
 		isOwnRecipe: readonly(isOwnRecipe),
--- src/shared/Pagination/PaginationState.ts
+++ src/shared/Pagination/PaginationState.ts
@@ -1,25 +1,25 @@
 import { readonly, ref } from 'vue';
 
-const currentPage = ref(1);
-const nextPageExists = ref(false);
-
-function goToPreviousPage() {
-	currentPage.value -= 1;
-}
-
-function goToNextPage() {
-	currentPage.value += 1;
-}
-
-function goToPage(page: number) {
-	currentPage.value = page;
-}
-
-function checkIfNextPageExists(value: boolean) {
-	nextPageExists.value = value;
-}
-
 export default function usePaginationState() {
+	const currentPage = ref(1);
+	const nextPageExists = ref(false);
+
+	function goToPreviousPage() {
+		currentPage.value -= 1;
+	}
+
+	function goToNextPage() {
+		currentPage.value += 1;
+	}
+
+	function goToPage(page: number) {
+		currentPage.value = page;
+	}
+
+	function checkIfNextPageExists(value: boolean) {
+		nextPageExists.value = value;
+	}
+
 	return {
 		currentPage: readonly(currentPage),
 		nextPageExists: readonly(nextPageExists),
